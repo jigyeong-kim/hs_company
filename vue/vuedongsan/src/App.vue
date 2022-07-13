605c03477f1f5663ddc099a96e44ddb099ad0a7c
jigyeong-kim <68461953+jigyeong-kim@users.noreply.github.com>
2022-07-13 18:00:38 +0900

    0713

---
+++ vue/vuedongsan/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div class="menu">
+    <a href="">home</a>
+    <a href="">products</a>
+    <a href="">about</a>
+  </div>
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <div>
+    <h4 :style="스타일">{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
+  </div>
+
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      //데이터 보관함
+      price1 : 60,
+      price2 : 70,
+      스타일 : 'color : blue',
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu{background: darkslateblue;padding: 15px;border-radius: 5px;}
+.menu a{color: #fff;padding: 10px;}
+</style>
